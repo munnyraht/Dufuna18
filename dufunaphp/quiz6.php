45ee8cfe8de9881b93b64161fed05441a9b6a009
munnyraht <muniratsulaimon@gmail.com>
2018-10-07 16:11:33 +0100

    Introduction to php

---
+++ dufunaphp/quiz6.php
@@ -0,0 +1,7 @@
+<?php
+function sumandproduct( $a, $b){
+    $sum=$a+$b;
+    $product=$a*$b;
+    echo "The sum of a and b is  $sum <br> The product of a and b is $product";
+}
+sumandproduct(30,40);
