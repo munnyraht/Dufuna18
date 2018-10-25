e7dd9d56b961a97a01737be13a0880645bb630cd
munnyraht <muniratsulaimon@gmail.com>
2018-10-25 21:31:07 +0100

    HtmlForms

--- submissions/muniratsulaimon/HtmlForms/process.php
+++ submissions/muniratsulaimon/HtmlForms/process.php
@@ -25,7 +25,7 @@ header('Location:sucess.html' );
         if (!$conn){
             die( "connection failed" . "mysql connect error");
         }
-    $query = "INSERT into Signup( firstname, lastname,gender,email,phoneno,dateofbirth,country,password,confirmpassword,created_date)
+    $query = "INSERT into users( firstname, lastname,gender,email,phoneno,dateofbirth,country,password,confirmpassword,created_date)
                           VALUES('$fname','$lname','$gender','$email','$phoneno','$dob','$country','$password','$repassword', NOW())";
     $result =mysqli_query( $conn,$query);
      if (!$result){ 
